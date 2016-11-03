4d4aa8e7c551a4dab586dc4bfcd25eb2f164330a
mksmith1982 <mksmith1982@gmail.com>
2016-11-03 13:17:33 -0400

    passed first test on normal mode

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,18 @@
-function fizz () {
-  // TODO
-};
+function fizz (number) {
+ 
+ if (number % 3 === 0) {
+ 	return "Fizz";
+ }else if (number % 5 === 0) {
+ 	return "Buzz";
+ }else if (number % 15 === 0) {
+ 	return "FizzBuzz";
+ }else{
+ 	return number.toString();
+ }
+ };
+
+ 
+
 
 function fizzBuzz (start, end) {
   // Write fizzBuzz here once fizz is finished.
